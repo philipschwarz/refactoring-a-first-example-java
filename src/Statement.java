22c21cbbd47dea072a65a0a99716fa649ab1bde7
philipschwarz <philip.johann.schwarz@gmail.com>
2022-01-16 08:25:41 +0000

    add htmlStatement and renderHtml (after moving usd to the top level so that renderHtml can use it) - also, add test assertion for htmlStatement

--- src/Statement.java
+++ src/Statement.java
@@ -3,7 +3,6 @@ import java.util.Currency;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.function.Function;
 
 public class Statement {
 
@@ -11,25 +10,42 @@ public class Statement {
     return renderPlainText(CreateStatementData.createStatementData(invoice,plays));
   }
 
+  static String htmlStatement(Invoice invoice, Map<String, Play> plays) {
+    return renderHtml(CreateStatementData.createStatementData(invoice, plays));
+  }
+  
   static String renderPlainText(StatementData data) {
-
-    Function<Integer,String> usd = aNumber -> {
-      final var formatter = NumberFormat.getCurrencyInstance(Locale.US);
-      formatter.setCurrency(Currency.getInstance(Locale.US));
-      return formatter.format(aNumber);
-    };
-
     var result = "Statement for " + data.customer() + "\n";
     for(EnrichedPerformance perf : data.performances()) {
-      result += "  " + perf.play().name() + ": " + usd.apply(perf.amount()/100)
+      result += "  " + perf.play().name() + ": " + usd(perf.amount()/100)
                      + " (" + perf.audience() + " seats)\n";
     }
 
-    result += "Amount owed is " + usd.apply(data.totalAmount()/100) + "\n";
+    result += "Amount owed is " + usd(data.totalAmount()/100) + "\n";
     result += "You earned " + data.totalVolumeCredits() + " credits\n";
     return result;
   }
 
+  static String renderHtml(StatementData data) {
+    var result = "<h1>Statement for "+data.customer()+"</h1>\n";
+    result += "<table>\n";
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>\n";
+    for (EnrichedPerformance perf : data.performances()) {
+      result += "<tr><td>" + perf.play().name() + "</td><td>" + perf.audience() + "</td>";
+      result += "<td>" + usd(perf.amount() / 100) + "</td></tr>\n";
+    }
+    result += "</table>\n";
+    result += "<p>Amount owed is <em>" + usd(data.totalAmount()/100) + "</em></p>\n";
+    result += "<p>You earned <em>"+ data.totalVolumeCredits() +"</em> credits</p>\n";
+    return result;
+  }
+
+  static String usd(int aNumber) {
+    final var formatter = NumberFormat.getCurrencyInstance(Locale.US);
+    formatter.setCurrency(Currency.getInstance(Locale.US));
+    return formatter.format(aNumber);
+  }
+  
   static final List<Invoice> invoices =
     List.of(
       new Invoice(
@@ -52,6 +68,19 @@ public class Statement {
         Othello: $500.00 (40 seats)
       Amount owed is $1,730.00
       You earned 47 credits
+      """
+    )) throw new AssertionError();
+    if (!Statement.htmlStatement(invoices.get(0), plays).equals(
+      """
+      <h1>Statement for BigCo</h1>
+      <table>
+      <tr><th>play</th><th>seats</th><th>cost</th></tr>
+      <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>
+      <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>
+      <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>
+      </table>
+      <p>Amount owed is <em>$1,730.00</em></p>
+      <p>You earned <em>47</em> credits</p>
       """
     )) throw new AssertionError();
   }
