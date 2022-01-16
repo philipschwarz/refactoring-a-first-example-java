8a18634460a98245fb4325479f250bd4f65b8d73
philipschwarz <philip.johann.schwarz@gmail.com>
2022-01-16 08:39:25 +0000

    Copy the logic of amountFor to a new PerformanceCalculator function called amount, and adjust it to fit in its new home.

--- src/Domain.java
+++ src/Domain.java
@@ -5,20 +5,38 @@ record Play(String name, String type) { }
 record Performance(String playID, int audience) { }
 
 record EnrichedPerformance(
-    String playID,
-    Play play,
-    int audience,
-    Integer amount,
-    Integer volumeCredits
+  String playID,
+  Play play,
+  int audience,
+  Integer amount,
+  Integer volumeCredits
 ) { }
 
 record Invoice(String customer, List<Performance> performances) { }
 
 record StatementData(
-    String customer,
-    List<EnrichedPerformance> performances,
-    Integer totalAmount,
-    Integer totalVolumeCredits
+  String customer,
+  List<EnrichedPerformance> performances,
+  Integer totalAmount,
+  Integer totalVolumeCredits
 ) { }
 
-record PerformanceCalculator(Performance performance, Play play) { }
+record PerformanceCalculator(Performance performance, Play play) {
+  int amount() {
+    var result = 0;
+    switch (play.type()) {
+      case "tragedy" -> {
+        result = 40_000;
+        if (performance.audience() > 30) 
+          result += 1_000 * (performance.audience() - 30); }
+      case "comedy" -> {
+        result = 30_000;
+        if (performance.audience() > 20) 
+          result += 10_000 + 500 * (performance.audience() - 20);
+        result += 300 * performance.audience(); }
+      default -> throw new IllegalArgumentException(
+            "unknown type " + play.type());
+    }
+    return result;
+  }
+}
